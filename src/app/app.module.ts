5b1e9b25d12c9e33121f4dc0d2a89b51872b395b
Cond Puin <conpuin1@gmail.com>
2022-05-24 09:58:37 -0500

    CARGA24052022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -18,6 +20,8 @@ import { ResumenComponent } from './components/resumen/resumen.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -1,15 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
-  styleUrls: ['./contacto.component.scss']
+  styleUrls: ['./contacto.component.scss'],
 })
 export class ContactoComponent implements OnInit {
+  dataset: Details = {
+    fname: '',
+    email: '',
+    comment: '',
+  };
 
-  constructor() { }
+  constructor(private https: HttpClient) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log("Ando por aquí.....");
+    this.https.post<Details>('http://localhost:8080/testapp/getdetails',this.dataset)
+      .subscribe((res) => {
+        this.dataset = res;
+        console.log("Esto es:   " + this.dataset);
+        alert('Email Sent successfully');
+        this.dataset.fname = '';
+        this.dataset.email = '';
+        this.dataset.comment = '';
+      });
   }
+}
+interface Details {
+  fname: string;
+  email: string;
+  comment: string;
 
 }
